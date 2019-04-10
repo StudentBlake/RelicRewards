96c90165dfcf4b255e73fe1cb64def9d965ace59
StudentBlake <6874208+StudentBlake@users.noreply.github.com>
2019-04-10 15:28:36 -0400

    Conform to MS spacing standard

--- RelicRewards/Main.Designer.cs
+++ RelicRewards/Main.Designer.cs
@@ -1,5 +1,7 @@
-namespace RelicRewards {
-    partial class Main {
+namespace RelicRewards
+{
+    partial class Main
+    {
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -9,8 +11,10 @@ namespace RelicRewards {
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing) {
-            if (disposing && (components != null)) {
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
                 components.Dispose();
             }
             base.Dispose(disposing);
@@ -22,7 +26,8 @@ namespace RelicRewards {
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent() {
+        private void InitializeComponent()
+        {
             this.LB_PlatDucats = new System.Windows.Forms.Label();
             this.LB_Part1 = new System.Windows.Forms.Label();
             this.LB_Part2 = new System.Windows.Forms.Label();
